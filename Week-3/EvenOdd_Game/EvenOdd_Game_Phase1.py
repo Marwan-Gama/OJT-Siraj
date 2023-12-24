a747dd05f9a5459a5e5a0617e06ddbf172aad4ec
marwan-gama <marwanabugama2000@gmail.com>
2023-12-24 14:10:49 +0200

    EvenOdd Game - write the code with out using class

--- Week-3/EvenOdd_Game/EvenOdd_Game_Phase1.py
+++ Week-3/EvenOdd_Game/EvenOdd_Game_Phase1.py
@@ -1,31 +1,29 @@
 import random
 
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
+def Player(name):
+    return {"name":name , "score":0}
 
 def play_round(player1, player2):
     number = random.randint(-5, 13)
     if number % 2 == 0:
-        player1.score += 1
-        print(f"Round: {player1.name} scored!")
+        player1['score'] += 1
+        print(f"Round: {player1['name']} scored!")
     else:
-        player2.score += 1
-        print(f"Round: {player2.name} scored!")
-    print(f"Status: {player1.name} {player1.score}, {player2.name} {player2.score}")
+        player2['score'] += 1
+        print(f"Round: {player2['name']} scored!")
+    print(f"Status: {player1['name']} {player1['score']}, {player2['name']} {player2['score']}")
 
 def play_game():
     player1_name = input("Enter Player 1's name: ")
     player2_name = input("Enter Player 2's name: ")
     player1 = Player(player1_name)
     player2 = Player(player2_name)
-    while player1.score < 3 and player2.score < 3:
+    while player1['score'] < 3 and player2['score'] < 3:
         play_round(player1, player2)
-    if player1.score == 3:
-        print(f"{player1.name} Wins!")
+    if player1['score'] == 3:
+        print(f"{player1['name']} Wins!")
     else:
-        print(f"{player2.name} Wins!")
+        print(f"{player2['name']} Wins!")
 
 if __name__ == "__main__":
     play_game()
